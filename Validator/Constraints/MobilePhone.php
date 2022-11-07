cbb8acf6d3a31c8afb167dc46d030f4a1fe2b2a2
Robert Pajer <robert.pajer@tolino.pl>
2022-11-07 08:51:51 +0100

    Add suport for Symfony 6

--- Validator/Constraints/MobilePhone.php
+++ Validator/Constraints/MobilePhone.php
@@ -10,11 +10,20 @@ use Symfony\Component\Validator\Constraint;
  *
  * @author Robert Pajer
  */
+#[\Attribute(\Attribute::TARGET_PROPERTY | \Attribute::TARGET_METHOD | \Attribute::IS_REPEATABLE)]
 class MobilePhone extends Constraint
 {
     public const LENGTH_ERROR = '2a0039fb-01b9-43aa-8140-37057083932f';
     public const INVALID_NUMBER_ERROR = '7611b63f-4ff2-44ca-959a-51fb7385f703';
 
+    protected const ERROR_NAMES = [
+        self::LENGTH_ERROR => 'LENGTH_ERROR',
+        self::INVALID_NUMBER_ERROR => 'INVALID_NUMBER_ERROR',
+    ];
+
+    /**
+     * @deprecated since Symfony 6.1, use const ERROR_NAMES instead
+     */
     protected static $errorNames = [
         self::LENGTH_ERROR => 'LENGTH_ERROR',
         self::INVALID_NUMBER_ERROR => 'INVALID_NUMBER_ERROR',
@@ -22,4 +31,18 @@ class MobilePhone extends Constraint
 
     public $lengthMessage = 'This value should have exactly 9 characters';
     public $invalidNumberMessage = 'This value is not a valid Polish mobile number';
+
+
+    public function __construct(
+        array $options = null,
+        string $lengthMessage = null,
+        $invalidNumberMessage = null,
+        array $groups = null,
+        mixed $payload = null
+    ) {
+        parent::__construct($options ?? [], $groups, $payload);
+
+        $this->lengthMessage = $lengthMessage ?? $this->lengthMessage;
+        $this->invalidNumberMessage = $invalidNumberMessage ?? $this->invalidNumberMessage;
+    }
 }
